f09844391270ae2e630a6218090e0ef00b16b8bd
Brian Anderson <brian.briananderson@gmail.com>
2024-11-13 23:01:27 -0600

    cut load time to 1/3rd by commenting out telescope as i am not using it

--- init.lua
+++ init.lua
@@ -15,11 +15,11 @@ vim.cmd.colorscheme "catppuccin-mocha"
 -- This is the startup page plugin
 
 -- Telescope keybindings
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Open Nvim Tree
 vim.keymap.set('n', '<leader>t', ':NvimTreeFindFileToggle<CR>', {noremap = true, silent = true})
--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   { "github/copilot.vim" },
   { "psf/black", branch = "stable" },
   { "nvim-tree/nvim-tree.lua" },
-  { "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+--  { "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
   { "VonHeikemen/lsp-zero.nvim", branch = 'v4.x'},
   { "neovim/nvim-lspconfig" },
   { "hrsh7th/cmp-nvim-lsp" },
