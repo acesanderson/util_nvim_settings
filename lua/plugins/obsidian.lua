cbeea4334fe6d3621553a33e0739c744ddaab5f9
bianders <phat@biden.co.uk>
2024-11-26 12:28:42 -0600

    added conditional loading of obsidian ONLY if env variable discovered; this so I can use nvim settings wtihin docker containers

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,27 +1,28 @@
 local vault_path = os.getenv("OBSIDIAN_PATH")
-
-return {
-	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
-	lazy = true,
-	-- ft = "markdown",
-	event = {
-		"BufReadPre " .. vault_path .. "*.md",
-		"BufNewFile " .. vault_path .. "*.md",
-	},
-	dependencies = {
-		-- Required.
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "Morphy",
-				path = vault_path,
-			},
+if vault_path then
+	return {
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		-- ft = "markdown",
+		event = {
+			"BufReadPre " .. vault_path .. "*.md",
+			"BufNewFile " .. vault_path .. "*.md",
 		},
-		follow_url_func = function(url)
-			vim.inspect(vim.system({ "open", url }))
-		end,
-	},
-}
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "Morphy",
+					path = vault_path,
+				},
+			},
+			follow_url_func = function(url)
+				vim.inspect(vim.system({ "open", url }))
+			end,
+		},
+	}
+end
