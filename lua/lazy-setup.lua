a646960fb63b8dc4d477bf11dafb0d3d4a2844eb
Brian Anderson <brian.briananderson@gmail.com>
2024-12-09 18:19:17 -0600

    installed nvim.tmux

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -65,7 +65,25 @@ require("lazy").setup({
 	-- 		require("alpha").setup(require("alpha.themes.startify").config)
 	-- 	end,
 	-- },
-}, {
+{ 
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	},
+}, 
+},
+{
 	performance = {
 		cache = {
 			enabled = true,
