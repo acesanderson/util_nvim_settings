694202f6d172c8cbe87fa844a18ab66190b46f31
Brian Anderson <brian.briananderson@gmail.com>
2024-12-09 18:35:02 -0600

    pulled plugins out of lazy set up and into specific plugins files

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -11,52 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- Here we're moving the Noice configuration into the opts table
-			lsp = {
-				-- This section enhances how LSP documentation appears in your editor
-				override = {
-					-- Improves the conversion of LSP documentation to markdown
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					-- Enhances how markdown is styled in the documentation window
-					["vim.lsp.util.stylize_markdown"] = true,
-					-- Makes completion documentation look better when using nvim-cmp
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-	},
-	{
-		"rest-nvim/rest.nvim",
-		cmd = { "Rest" },
-	},
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBuffer",
-		},
-	},
-	{ "github/copilot.vim", event = "VeryLazy" },
 	-- { "bling/vim-bufferline" },
-	{ "akinsho/bufferline.nvim", event = "VeryLazy" },
 	{ import = "plugins" },
 	-- {
 	-- 	"goolord/alpha-nvim",
@@ -65,25 +20,7 @@ require("lazy").setup({
 	-- 		require("alpha").setup(require("alpha.themes.startify").config)
 	-- 	end,
 	-- },
-{ 
-	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-	},
-}, 
-},
-{
+}, {
 	performance = {
 		cache = {
 			enabled = true,
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- Here we're moving the Noice configuration into the opts table
+		lsp = {
+			-- This section enhances how LSP documentation appears in your editor
+			override = {
+				-- Improves the conversion of LSP documentation to markdown
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				-- Enhances how markdown is styled in the documentation window
+				["vim.lsp.util.stylize_markdown"] = true,
+				-- Makes completion documentation look better when using nvim-cmp
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+}
