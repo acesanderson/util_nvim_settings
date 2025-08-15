52aca0ec3126d13f6a8c09c16d45ebf1025bd8c6
Johnny Cakes <biden@phat.co.uk>
2025-08-14 19:05:10 -0500

    fixed textobjects for treesitter, and now can navigate funcs and classes with []

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,54 +1,23 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false, -- Last release is way too old and doesn't work on Windows
+	version = false,
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
-	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
-		ensure_installed = {
-			"lua", -- for neovim config
-			"vimdoc", -- for help files
-			"vim", -- for .vim files
-			"markdown", -- for markdown, obsidian files
-			"markdown_inline", -- for markdown, obsidian files
-			"python",
-			"javascript",
-			"json",
-			"jsonc",
-			"bash",
-			"regex",
-		},
-		-- Automatically install parsers when entering buffer
-		auto_install = true,
-
-		-- Install parsers synchronously (only applied to `ensure_installed`)
-		sync_install = false,
-
-		-- List of parsers to ignore
-		ignore_install = {},
-	},
 	config = function()
-		-- Define filetype to parser mapping
-		local parser_mapping = {
-			javascriptreact = { "tsx" }, -- Add this line for JSX files
-			typescript = { "typescript", "tsx" },
-			typescriptreact = { "tsx" },
-			yaml = { "yaml" },
-			html = { "html" },
-			css = { "css" },
-			sql = { "sql" },
-			-- Exclude Python since it's always loaded
-			python = {}, -- No need to list as it's always installed
-		}
-
-		-- Setup treesitter
 		require("nvim-treesitter.configs").setup({
-
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				"lua", "vimdoc", "vim", "markdown", "markdown_inline", 
+				"python", "javascript", "json", "jsonc", "bash", "regex",
+			},
+			auto_install = true,
+			sync_install = false,
+			ignore_install = {},
+			
 			incremental_selection = {
 				enable = true,
 				keymaps = {
@@ -58,68 +27,49 @@ return {
 					node_decremental = "<Leader>bd",
 				},
 			},
+			
 			textobjects = {
-				swap = {
-					enable = true,
-					swap_next = {
-						["<Leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<Leader>A"] = "@parameter.inner",
-					},
-				},
 				select = {
 					enable = true,
-
-					-- Automatically jump forward to textobj, similar to targets.vim
 					lookahead = true,
-
 					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
 						["af"] = "@function.outer",
 						["if"] = "@function.inner",
 						["ac"] = "@class.outer",
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						["ic"] = "@class.inner",
+						["as"] = "@scope",
 					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						["@parameter.outer"] = "v", -- charwise
-						["@function.outer"] = "v", -- charwise
-						["@class.outer"] = "<c-v>", -- charwise
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
 					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true or false
-					include_surrounding_whitespace = true,
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
 				},
 			},
-		})
-		-- Create autocmd for lazy loading other parsers
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				local ft = vim.bo.filetype
-				if parser_mapping[ft] then
-					for _, parser in ipairs(parser_mapping[ft]) do
-						vim.cmd("TSInstall " .. parser)
-					end
-				end
-			end,
 		})
 	end,
 }
